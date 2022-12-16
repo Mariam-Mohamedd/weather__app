950806cfffd20d9a1397e60c63bd967b9a946af9
Mariam Mohamed <104093467+Mariam-Mohamedd@users.noreply.github.com>
2022-12-16 16:39:10 +0200

    Add files via upload

---
+++ Weather-Journal-App-main/server.js
@@ -0,0 +1,37 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Dependencies
+const bodyParser = require('body-parser');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Post Route
+app.post('/add', (req, res) => {
+    projectData = req.body;
+    res.status(200).send(projectData)
+});
+
+// Callback function to complete GET '/all'
+app.get('/all', (req, res) => res.status(200).send(projectData));
+
+// Setup Server
+const port = 3000;
+app.listen(port, () =>
+    console.log(`server running on: http://localhost:${port}`)
+);
